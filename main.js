7aca8d571f155e66aab11884953779e651dc3325
Github1234fab <108588323+Github1234fab@users.noreply.github.com>
2022-07-21 13:26:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+{/* <div class="card8 cardsBackgroundContainerB PaddingInfoBloc2">
+      <div><div class="imgHeberg8"></div></div>
+
+      <div class="infoBloc ">
+        <h3>Au coeur de l'eau Chambre d'hôtes</h3>
+        <h4>Nuit à partir de 71€</h4>
+      <div class="etoiles"></div>
+
+    </div>
+  </div>
+
+  <div class="card9 cardsBackgroundContainerB PaddingInfoBloc2">
+    <div><div class="imgHeberg9"></div></div>
+
+    <div class="infoBloc">
+      <h3>Hôtel Tout bleu et Blanc</h3>
+      <h4>Nuit à partir de 68€</h4>
+    <div class="etoiles"></div>
+
+  </div>
+</div>
+</div>
+</div>
+</div> */}
+    
